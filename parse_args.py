eeb2dfa3a6ba6b7baffb090efd23e62ae215dff0
HackTestes <caio.hacktestes@gmail.com>
2024-02-14 14:49:30 -0300

    Argument parsing and unit tests

---
+++ parse_args.py
@@ -0,0 +1,153 @@
+class InvalidAction(Exception):
+    pass
+
+class InvalidOption(Exception):
+    pass
+
+class InvalidNumberSpeedPairParams(Exception):
+    pass
+
+class InvalidFanSpeed(Exception):
+    pass
+
+class InsufficientArgs(Exception):
+    pass
+
+class InvalidConfig(Exception):
+    pass
+
+
+class Configuration:
+
+    def __init__(self):
+        # This only supports one target gpu, use a process for each GPU (erros become isolated to each other) 
+        self.target_gpu = ""
+        self.action = ""
+        self.temp_speed_pair = []
+        self.curve_type = "fixed" # Currently for internal usage only (I want to later add calculation for lines and curves fuctions)
+        self.default_speed = 50 # Percentage
+        self.time_interval = 1.0 # In seconds
+
+class TempSpeedPair:
+
+    def __init__(self, temperature, speed):
+
+        self.temperature = temperature
+        self.speed = speed # Percentage!
+
+    # The sorting is based on the temperature
+    def __lt__(self, other):
+        if (self.temperature <= other.temperature):
+            return True
+
+        else:
+            return False
+
+    # Important for testing
+    def __eq__(self, other):
+        if (self.temperature == other.temperature and self.speed == other.speed):
+            return True
+
+        else:
+            return False
+        
+# Some sane checks (in case the user makes a bad config by accident)
+def validate_config(config):
+
+    if config.target_gpu == '':
+        print("You did not select a target GPU")
+        raise InvalidConfig("No GPU was selected")
+
+    #if len(config.temp_speed_pair) == 0:
+    #    print("You did not create fan points (see --speed-pairs)")
+    #    raise InvalidConfig("Has no fan curve")
+
+
+def parse_cmd_args(args):
+    
+    configuration = Configuration()
+
+    if len(args) == 1:
+        print(f'You must pass more argument')
+        raise InsufficientArgs("No action was supplied")
+
+    # You can always ignore the first argument, since it is the program itself
+    # Get the second arg, which is the action
+    action = args[1]
+
+    # The action names are decoupled from the cmd interface, allowing for flexibility
+    if (action == 'help'):
+        configuration.action = 'help'
+        return configuration # It should stop here, ignore all other args
+
+    elif (action == 'list'):
+        configuration.action = 'list'
+        return configuration # It should stop here, ignore all other args
+
+    elif (action == 'fan-control'):
+        configuration.action = 'fan-control'
+
+    else:
+        print(f'Invalid action: {action}')
+        raise InvalidAction("The action passed as argument is incorrect")
+
+
+    # You can safely ignore the actions here
+    i = 2
+    while(i < len(args)):
+
+        arg = args[i]
+
+        if (arg == '--target' or arg == '-t'):
+            configuration.target_gpu = args[i+1]
+            i += 1 # Skip the next iteration
+
+        elif (arg == '--speed-pair' or arg == '-sp'):
+
+            # Think of as points in a graph (speed % x temp °C)
+            speed_points = args[i+1].split(',')
+
+            for speed_pair_str in speed_points:
+    
+                speed_pair = speed_pair_str.split(':')
+
+                if (len(speed_pair) != 2):
+                    print('Invalid number of speed pair parameters')
+                    raise InvalidNumberSpeedPairParams("You can only set temperature and target speed at a time")
+
+                temp = int(speed_pair[0])
+                speed = int(speed_pair[1])
+
+                if (speed > 100):
+                    print(f'The fan speed only goes up to 100%. You choose {speed}')
+                    raise InvalidFanSpeed(f'The fan speed only goes up to 100%. You choose {speed}')
+
+                if (speed < 0):
+                    print(f'The fan speed cannot be lower than 0%. You choose {speed}')
+                    raise InvalidFanSpeed(f'The fan speed cannot be lower than 0%. You choose {speed}')
+
+                configuration.temp_speed_pair.append( TempSpeedPair(temp, speed) )
+
+            i += 1 # Skip the next iteration
+
+        elif (arg == '--default-speed' or arg == '-ds'):
+            configuration.default_speed = int(args[i+1])
+            i += 1 # Skip the next iteration
+
+        elif (arg == '--time-interval' or arg == '-t'):
+            configuration.time_interval = float(args[i+1])
+            i += 1 # Skip the next iteration
+
+        else:
+            print(f'Invalid option: {arg}')
+            raise InvalidOption('The option given was invalid')
+
+        # Change iteration
+        i += 1
+
+    # Organizing the array before sending the configuration
+    configuration.temp_speed_pair.sort(reverse=True)
+
+    validate_config(configuration)
+
+    return configuration
---
+++ tests.py
@@ -0,0 +1,151 @@
+import unittest
+import sys
+import parse_args
+
+# Test command: python.exe .\tests.py -b
+
+class TestMethods(unittest.TestCase):
+
+    def test_parse_args_inssuficient_args(self):
+        with self.assertRaises(parse_args.InsufficientArgs):
+            parse_args.parse_cmd_args(['.python_script'])
+
+    def test_parse_args_help(self):
+        config = parse_args.parse_cmd_args(['.python_script', 'help'])
+        self.assertEqual( config.action, 'help')
+
+    def test_parse_args_list(self):
+        config = parse_args.parse_cmd_args(['.python_script', 'list'])
+        self.assertEqual( config.action, 'list')
+
+    def test_parse_args_fan_control(self):
+        config = parse_args.parse_cmd_args(['.python_script', 'fan-control', '--target', 'RTX 4080'])
+        self.assertEqual( config.action, 'fan-control')
+
+    def test_parse_args_invalid_action(self):
+        with self.assertRaises(parse_args.InvalidAction):
+            parse_args.parse_cmd_args(['.python_script', 'invalid-action'])
+
+    def test_parse_args_action_ignore_rest(self):
+        config = parse_args.parse_cmd_args(['.python_script', 'help', '--target', 'RTX 4080'])
+        self.assertEqual( config.action, 'help')
+        self.assertEqual( config.target_gpu, '')
+
+        config = parse_args.parse_cmd_args(['.python_script', 'list', '--target', 'RTX 4080'])
+        self.assertEqual( config.action, 'list')
+        self.assertEqual( config.target_gpu, '')
+
+    def test_parse_args_option_target(self):
+        config = parse_args.parse_cmd_args(['.python_script', 'fan-control', '--target', 'RTX 4080'])
+        self.assertEqual( config.action, 'fan-control')
+        self.assertEqual( config.target_gpu, 'RTX 4080')
+
+        config = parse_args.parse_cmd_args(['.python_script', 'fan-control', '-t', 'RTX 3080'])
+        self.assertEqual( config.action, 'fan-control')
+        self.assertEqual( config.target_gpu, 'RTX 3080')
+
+    def test_parse_args_option_default_speed(self):
+        config = parse_args.parse_cmd_args(['.python_script', 'fan-control', '--default-speed', '36', '-t', 'RTX 3080'])
+        self.assertEqual( config.action, 'fan-control')
+        self.assertEqual( config.default_speed, 36)
+
+        config = parse_args.parse_cmd_args(['.python_script', 'fan-control', '-ds', '27', '-t', 'RTX 3080'])
+        self.assertEqual( config.action, 'fan-control')
+        self.assertEqual( config.default_speed, 27)
+    
+        config = parse_args.parse_cmd_args(['.python_script', 'fan-control', '-t', 'RTX 3080'])
+        self.assertEqual( config.action, 'fan-control')
+        self.assertTrue( config.default_speed >= 30) # Fan speed must never default for a value lower than 30%, except for when user explicitly wants to
+
+    def test_parse_args_option_time_interval(self):
+        config = parse_args.parse_cmd_args(['.python_script', 'fan-control', '--time-interval', '5', '-t', 'RTX 3080'])
+        self.assertEqual( config.action, 'fan-control')
+        self.assertEqual( config.time_interval, 5.0)
+
+        config = parse_args.parse_cmd_args(['.python_script', 'fan-control', '--time-interval', '0.5', '-t', 'RTX 3080'])
+        self.assertEqual( config.action, 'fan-control')
+        self.assertEqual( config.time_interval, 0.5)
+
+        config = parse_args.parse_cmd_args(['.python_script', 'fan-control', '-t', 'RTX 3080'])
+        self.assertEqual( config.action, 'fan-control')
+        self.assertTrue( config.time_interval <= 1) # Default should never be higher than 1s, unless the user states so
+
+    def test_parse_args_temp_speed_pair(self):
+        config = parse_args.parse_cmd_args(['.python_script', 'fan-control', '--speed-pair', '0:0,10:30,20:50,35:75,40:100', '-t', 'RTX 3080'])
+
+        expected_output = [
+            parse_args.TempSpeedPair(40, 100),
+            parse_args.TempSpeedPair(35, 75),
+            parse_args.TempSpeedPair(20, 50),
+            parse_args.TempSpeedPair(10, 30),
+            parse_args.TempSpeedPair(0, 0),
+        ]
+        self.assertEqual(expected_output, config.temp_speed_pair)
+
+    def test_parse_args_temp_speed_pair_sort(self):
+        config = parse_args.parse_cmd_args(['.python_script', 'fan-control', '--speed-pair', '40:100,20:50,10:30,35:75', '-t', 'RTX 3080'])
+
+        expected_output = [
+            parse_args.TempSpeedPair(40, 100),
+            parse_args.TempSpeedPair(35, 75),
+            parse_args.TempSpeedPair(20, 50),
+            parse_args.TempSpeedPair(10, 30),
+        ]
+        self.assertEqual(expected_output, config.temp_speed_pair)
+
+    def test_parse_args_temp_speed_pair_empty_list(self):
+
+        with self.assertRaises(IndexError):
+            parse_args.parse_cmd_args(['.python_script', 'fan-control', '--speed-pair'])
+
+    def test_parse_args_temp_speed_pair_invalid_list(self):
+
+        with self.assertRaises(parse_args.InvalidNumberSpeedPairParams):
+            parse_args.parse_cmd_args(['.python_script', 'fan-control', '--speed-pair', ''])
+
+        with self.assertRaises(parse_args.InvalidNumberSpeedPairParams):
+            parse_args.parse_cmd_args(['.python_script', 'fan-control', '--speed-pair', '10-20'])
+
+        with self.assertRaises(ValueError):
+            parse_args.parse_cmd_args(['.python_script', 'fan-control', '--speed-pair', '10-20:10-20'])
+        
+        with self.assertRaises(ValueError):
+            parse_args.parse_cmd_args(['.python_script', 'fan-control', '--speed-pair', '10-20:10-20,10-20:10-20'])
+
+    def test_parse_args_temp_speed_pair_invalid_fan_speed(self):
+
+        with self.assertRaises(parse_args.InvalidFanSpeed):
+            parse_args.parse_cmd_args(['.python_script', 'fan-control', '--speed-pair', '10:120'])
+
+        with self.assertRaises(parse_args.InvalidFanSpeed):
+            parse_args.parse_cmd_args(['.python_script', 'fan-control', '--speed-pair', '10:-100'])
+
+    def test_parse_args_invalid_option(self):
+
+        with self.assertRaises(parse_args.InvalidOption):
+            parse_args.parse_cmd_args(['.python_script', 'fan-control', '--invalid-option', '10:120', '-t', 'RTX 3080'])
+
+    def test_parse_args_real_cmd(self):
+
+        config = parse_args.parse_cmd_args(['.python_script', 'fan-control', '--target', 'RTX 4080', '--speed-pair', '0:0,20:35,30:50,40:100', '--time-interval', '0.5'])
+
+        self.assertEqual(config.action, 'fan-control')
+        self.assertEqual(config.target_gpu, 'RTX 4080')
+        self.assertEqual(config.time_interval, 0.5)
+        expected_output = [
+            parse_args.TempSpeedPair(40, 100),
+            parse_args.TempSpeedPair(30, 50),
+            parse_args.TempSpeedPair(20, 35),
+            parse_args.TempSpeedPair(0, 0),
+        ]
+        self.assertEqual(config.temp_speed_pair, expected_output)
+
+    def test_parse_args_sane_checks(self):
+
+        with self.assertRaises(parse_args.InvalidConfig):
+            parse_args.parse_cmd_args(['.python_script', 'fan-control']) 
+
+
+
+if __name__ == '__main__':
+    unittest.main()
