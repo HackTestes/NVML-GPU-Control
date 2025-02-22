3a8a61ea6e64f03e4b8686dc70321eaf6b43a788
HackTestes <caio.hacktestes@gmail.com>
2025-02-22 15:54:51 -0300

    Saving experimental tests to see if I could get VRAM temp readings (it didn't work)

--- src/experimental.py
+++ src/experimental.py
@@ -5,12 +5,25 @@ pynvml.nvmlInit()
 
 gpu = pynvml.nvmlDeviceGetHandleByUUID('GPU-80285976-b824-419f-d246-35946b3bb2a6')
 
-policy = c_uint(0)
+#policy = c_uint(0)
+#print(policy)
+#
+#pynvml.nvmlDeviceGetFanControlPolicy_v2(gpu, 0, byref(policy))
+#print(policy)
 
-print(policy)
+sensors = pynvml.nvmlDeviceGetThermalSettings(gpu, 2)
 
-pynvml.nvmlDeviceGetFanControlPolicy_v2(gpu, 0, byref(policy))
+for sensor in sensors:
+    print(f"Controller: {sensor.controller}")
+    print(f"defaultMinTemp: {sensor.controller}")
+    print(f"defaultMaxTemp: {sensor.controller}")
+    print(f"currentTemp: {sensor.controller}")
+    print(f"target: {sensor.controller}")
 
-print(policy)
+temp = pynvml.nvmlDeviceGetTemperatureV(gpu, 0)
+print(f"nvmlDeviceGetTemperatureV: {temp}")
+
+temp = pynvml.nvmlDeviceGetTemperatureV1(gpu, 0)
+print(f"nvmlDeviceGetTemperatureV1: {temp}")
 
 pynvml.nvmlShutdown()
