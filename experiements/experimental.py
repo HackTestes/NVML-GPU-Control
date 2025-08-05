77f593e95b7386087113da0271e97f26371bce8e
HackTestes <caio.hacktestes@gmail.com>
2025-08-05 19:51:53 -0300

    Added new experiments with NVML_FI_DEV_MEMORY_TEMP (doesn't work)

--- experiements/experimental.py
+++ experiements/experimental.py
@@ -4,21 +4,6 @@ import subprocess
 import sys
 from ctypes import *
 
-
-print(f'argv: {sys.argv}')
-print(f"os.execv(`{sys.executable}`, {sys.argv})")
-print(f"'{sys.executable}' {' '.join(sys.argv)}")
-
-restart = input("Restart? Y/N\n")
-
-if restart == "Y":
-    print("Restarting now...")
-    os.execv(sys.executable, ['python3'] + sys.argv)
-    #os.system(f'"{sys.executable}" {" ".join(sys.argv)}')
-    #subprocess.run([sys.executable] + sys.argv, timeout=1)
-    
-quit()
-
 pynvml.nvmlInit()
 
 gpu = pynvml.nvmlDeviceGetHandleByUUID('GPU-80285976-b824-419f-d246-35946b3bb2a6')
@@ -29,7 +14,7 @@ gpu = pynvml.nvmlDeviceGetHandleByUUID('GPU-80285976-b824-419f-d246-35946b3bb2a6
 #pynvml.nvmlDeviceGetFanControlPolicy_v2(gpu, 0, byref(policy))
 #print(policy)
 
-sensors = pynvml.nvmlDeviceGetThermalSettings(gpu, 2)
+sensors = pynvml.nvmlDeviceGetThermalSettings(gpu, 82)
 
 for sensor in sensors:
     print(f"Controller: {sensor.controller}")
@@ -43,5 +28,10 @@ print(f"nvmlDeviceGetTemperatureV: {temp}")
 
 temp = pynvml.nvmlDeviceGetTemperatureV1(gpu, 0)
 print(f"nvmlDeviceGetTemperatureV1: {temp}")
+
+temp = pynvml.nvmlDeviceGetFieldValues(gpu, [pynvml.NVML_FI_DEV_MEMORY_TEMP])
+print(f"nvmlDeviceGetFieldValues info: {temp[0]}")
+print(f"nvmlDeviceGetFieldValues (0 is success): {temp[0].nvmlReturn}")
+print(f"nvmlDeviceGetFieldValues : {temp[0].value.uiVal}")
 
 pynvml.nvmlShutdown()
